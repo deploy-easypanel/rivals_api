e62f953e9cef5bd9e663dce6308f049a5b08e0a5
saulojustiniano1 <saulojustiniano1@gmail.com>
2025-06-30 17:09:51 -0300

    feat: remove authentication routes and services for refactoring

--- src/utils/jwt.ts
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const generateToken = (payload: object): string => {
-  return jwt.sign(payload, process.env.JWT_SECRET as string, {
-    expiresIn: '24h',
-  });
-};
