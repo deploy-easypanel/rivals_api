6a7573fdb940e9b924de8619fdb182f34b8e621a
saulojustiniano1 <saulojustiniano1@gmail.com>
2025-06-30 17:10:38 -0300

    feat: update environment variables and add database schema for admins and banners

---
+++ ducks_db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS rivals_admins (
+  id SERIAL PRIMARY KEY,
+  email TEXT UNIQUE NOT NULL,
+  senha TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS rivals_banners (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  subtitle TEXT NOT NULL,
+  paragraph TEXT NOT NULL,
+  color_start VARCHAR(7) NOT NULL,
+  color_end VARCHAR(7) NOT NULL,
+  data_torneio TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP DEFAULT NOW()
+);
+
